fbee73be9bef50141696627b4a3e1881feda9d79
RozeU <rozeu668@gmail.com>
2023-06-10 20:55:32 +0300

    Pievienots skripts kurš atļauj nomainīt ainas

---
+++ C#Quiz/Assets/Skripti/AinuParsledzejs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AinuParsledzejs : MonoBehaviour {
+
+	//Párslédz ainu uz sákuma ekrańu
+	public void UzSakumaEkranu(){
+		SceneManager.LoadScene ("SakumaEkrans", LoadSceneMode.Single);
+	}
+
+	//Párslédz ainu uz testu
+	public void UzQuizAinu(){
+		SceneManager.LoadScene("Quiz", LoadSceneMode.Single);
+	}
+
+	//Atsákt testu no jauna
+	public void AtsaktTestu(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+	//Iziet no spéles
+	public void IzietNoSpeles(){
+		Application.Quit ();
+	}
+}
